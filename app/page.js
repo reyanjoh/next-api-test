456ac8642b8a6bb08dc89934a920f611d9265634
Reyan <reyanjohnnarvaja@gmail.com>
2024-10-27 05:25:00 +0800

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.30.1
      project name = efwfwe
      framework = next
      framework cli = create-next-app@14.2.5
      package manager = npm@10.8.2
      wrangler = wrangler@3.82.0
      git = 2.40.0.windows.1

--- app/page.js
+++ app/page.js
@@ -1,48 +1,95 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-
-const getTeam = async () =>{
-  const res = await fetch(`${process.env.API}/hello`)
-  return res.json()  
-}
-
-export default async function Home() {
-
-
-  
-
-const team = await getTeam()
-
+export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-        {team.map(team =>{
-          return(
-            <a key={team._id} href={`/staff/${team._id}`} className={styles.teamProfileCard}>
-              <div className={styles.teamProfile}>
-                <img src={team.profile} alt="" />
-              </div>
-              <div className={styles.teamInfo}>
-              <span className={styles.teamInfoSpanName}>{team.fName}, {team.lName}</span>
-              <span className={styles.teamInfoSpan}>{team.branchLocation}</span>
-              </div>
-            </a>
-          )
-        })}
-        {/* {{team.map(team =>{                  
-          return(
-            <a key={team._id} href={`/staff/${team._id}`} className={styles.teamProfileCard}>
-              <div className={styles.teamProfile}>
-                <img src={team.profile} alt="" />
-              </div>
-              <div className={styles.teamInfo}>
-              <span className={styles.teamInfoSpanName}>{team.fName}, {team.lName}</span>
-              <span className={styles.teamInfoSpan}>{team.branchLocation}</span>
-              </div>
-            </a>
-          )
-        })}} */}
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
   );
 }
